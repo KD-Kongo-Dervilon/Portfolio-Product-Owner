b1385b3e249641c4541683d6130c2b0a4716b958
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2023-12-18 09:26:45 +0100

    importation de logo

--- portfolio-product-owner/src/components/header/Header.jsx
+++ portfolio-product-owner/src/components/header/Header.jsx
@@ -3,9 +3,42 @@ import "./header.css";
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className="header">
+            <nav className='"nav container'>
+                <a href="index.html" className="nav__logo">KD Dervilon</a>
+
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="uil uil-estate nav-icon">Home</i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="uil uil-user nav-icon">About</i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="uil uil- nav-icon"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="uil uil- nav-icon"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="uil uil- nav-icon"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </header>
     )
 }
 
