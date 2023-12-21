dad5b7d2e98b70aa90a04fd71e3286827c0978c4
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2023-12-21 15:01:02 +0100

    Mise en place de home et data ,social

--- portfolio-product-owner/src/components/home/Social.jsx
+++ portfolio-product-owner/src/components/home/Social.jsx
@@ -5,18 +5,13 @@ const Social = () => {
         <div>
             <div className="home_social">
                 <a href="https://www.linkedin.com/in/dervilon-m-bissi-kongo-ab3546250/" className="home_social-icon" target="_blank">
-                    <svg width="50px" height="50px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">   
-                        <rect x="2" y="2" width="28" height="28" rx="14" fill="#1275B1"/>
-                        <path d="M12.6186 9.69215C12.6186 10.6267 11.8085 11.3843 10.8093 11.3843C9.81004 11.3843 9 10.6267 9 9.69215C9 8.7576 9.81004 8 10.8093 8C11.8085 8 12.6186 8.7576 12.6186 9.69215Z" fill="white"/>
-                        <path d="M9.24742 12.6281H12.3402V22H9.24742V12.6281Z" fill="white"/>
-                        <path d="M17.3196 12.6281H14.2268V22H17.3196C17.3196 22 17.3196 19.0496 17.3196 17.2049C17.3196 16.0976 17.6977 14.9855 19.2062 14.9855C20.911 14.9855 20.9008 16.4345 20.8928 17.5571C20.8824 19.0244 20.9072 20.5219 20.9072 22H24V17.0537C23.9738 13.8954 23.1508 12.4401 20.4433 12.4401C18.8354 12.4401 17.8387 13.1701 17.3196 13.8305V12.6281Z" fill="white"/>
-                    </svg>
+                    <i class="uil-linkedin-alt"></i>
                 </a>
                 <a href="https://dribbble.com/designers?tab=results&search%5Bname%5D=Product%20Owner&search%5BsavedSearchId%5D=325548&search%5Bspecialties%5D%5B%5D=2&search%5BexperienceLevels%5D%5B%5D=1–2%20years&search%5BbookmarkCount%5D=0&search%5BhourlyRateMin%5D=0&search%5BhourlyRateMax%5D=0&search%5BworkType%5D=freelance&search%5BskipUpsell%5D=true&search%5BsearchUid%5D=18499051-1702990974423" className="home_social-icon" target="_blank">
-
+                    <i class="uil uil-dribbble"></i>
                 </a>
                 <a href="https://github.com/KD-Kongo-Dervilon" className="home_social-icon" target="_blank">
-        
+                    <i class="uil uil-github-alt"></i>
                 </a>
             </div>
         </div>
