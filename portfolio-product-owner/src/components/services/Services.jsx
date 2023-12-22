bed8d91882665f4326a589c2426131d049a6a30b
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2023-12-22 19:20:11 +0100

    update services.css

--- portfolio-product-owner/src/components/services/Services.jsx
+++ portfolio-product-owner/src/components/services/Services.jsx
@@ -1,23 +1,142 @@
 import React from "react";
+import "./services.css";
 
 const Services = () => {
     return (
     <section className="services section" id="services">
-        <h2 className="section__title"> Services </h2>{" "}
+        <h2 className="section__title"> Services </h2>
         <span className="section__subtitle"> Ce que J 'offre</span>
 
         <div className="services__container container grid">
             <div className="services__content">
                 <div>
-                    <i className="uil iul services__icon"> </i>
-                    <h3 className="services__title"> </h3>
+                    <i class='bx bx-package services__icon'></i>
+                    <h3 className="services__title"> Gestion de Produit </h3>
                 </div>
 
-                <span className="services__button"> En savoir + <i className="uil iul-arrow-right services__button-icon"> </i></span>
+                <span className="services__button"> En savoir + <i className="uil uil-arrow-right services__button-icon"> </i></span>
 
                 <div className="services__modal">
                     <div className="services__modal-content">
-                        <i className="uil iul-times services__modal-close"> </i>
+                        <i className="uil uil-times services__modal-close"> </i>
+                        <h3 className="services__modal-title"> </h3>
+                        <p className="services__modal-description">
+                            En un an d 'expérience en tant que étudiant en gestion de
+                            projet, j 'ai mené à bien la réalisation de 12 projets dans le
+                            cadre de ma formation en tant que Product Manager.Ces
+                            réalisations ont été accompagnées d 'avis positifs sur la
+                            qualité des livrables ainsi que sur la qualité de mon travail.{" "}
+                        </p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-web-grid services__icon"> </i>
+                    <h3 className="services__title"> Web Designer </h3>
+                </div>
+
+                <span className="services__button"> En savoir + <i className="uil uil-arrow-right services__button-icon"> </i></span>
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"> </i>
+                        <h3 className="services__modal-title"> </h3>
+                        <p className="services__modal-description">
+                            En un an d 'expérience en tant que étudiant en gestion de
+                            projet, j 'ai mené à bien la réalisation de 12 projets dans le
+                            cadre de ma formation en tant que Product Manager.Ces
+                            réalisations ont été accompagnées d 'avis positifs sur la
+                            qualité des livrables ainsi que sur la qualité de mon travail.{" "}
+                        </p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"> </i>
+                                <p className="services__modal-info">
+                                    I develo the user interface
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-arrow services__icon"> </i>
+                    <h3 className="services__title"> UI/UX Designer </h3>
+                </div>
+
+                <span className="services__button"> En savoir + <i className="uil uil-arrow-right services__button-icon"> </i></span>
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"> </i>
                         <h3 className="services__modal-title"> </h3>
                         <p className="services__modal-description">
                             En un an d 'expérience en tant que étudiant en gestion de
