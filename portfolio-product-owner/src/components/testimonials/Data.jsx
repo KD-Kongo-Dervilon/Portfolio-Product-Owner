e00c6b5ed16fd187a066e4b0d74ca8c3faed3cd7
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2024-01-11 18:41:44 +0100

    mise en place ,des avis , import de swiper ,et mise en place de data

---
+++ portfolio-product-owner/src/components/testimonials/Data.jsx
@@ -0,0 +1,35 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+import Image4 from "../../assets/testimonial4.png";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        author: "Maîté Chamerois",
+        title: "Gérez un sprint du début à la fin",
+        description:"La compréhension du produit est bonne,bonne capacité de répartition du projet en itération"
+    },
+    {
+        id: 2,
+        image: Image2,
+        author: "Daryan Colard",
+        title: "Managez des imprévus durant un sprint",
+        description:" Soutenance satisfaisante permettant de présenter la démarche suivie pour répondre aux différents problèmes rencontrés.Les étapes sont expliquées, les outils utilisés détaillés notamment à travers les livrables réalisés."
+    },
+    {
+        id: 3,
+        image: Image3,
+        author: "Romain Dassy",
+        title: "Entretenez votre Product Backlog",
+        description:"La présentation montre les différents étapes de la review (ce qui a été fait, démo, et les prochaines releases)."
+    },
+    {
+        id: 4,
+        image: Image4,
+        author: "Cédric Compagnon",
+        title: "Convertissez les visiteurs d'un site e-commerce en prospects",
+        description:"Sans explication fortuite, la posture de l'apprenant concernant les livrables est à la hauteur de ce qui est demandé."
+    },
+]
--- portfolio-product-owner/src/components/testimonials/Testimonials.jsx
+++ portfolio-product-owner/src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
+import "./testimonial.css";
+import { Data } from "./Data";
+
+// Import Swiper React components 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper Styles
+import "swiper/css/pagination";
+
 
 const Testimonials = () => {
     return (
         <section className="testimonial container section">
             <h2 className="section__title"> Les avis sur mes Projets </h2>
-            <span className="section__subtitle"> Les témoignages laissées </span>
+            <span className="section__subtitle"> Les évaluation laissées </span>
 
-            <div className="testimonial__container"></div>
+            <div className="testimonial__container">
+                {Data.map(({id, image, author, title, description}) => {
+                    return (
+                        <div className="testimonial__card" key={id}>
+                            <img src={image} alt="" className="testimonial__img" />
+                            <h3 className="testimonial__name">{title}</h3>
+                            <span className="testimonial__author">{author}</span>
+                            <p className="testimonial__description">{description}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </section>
     )
 }
