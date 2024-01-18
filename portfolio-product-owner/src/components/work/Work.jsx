c0a7aecaed67d996b9cd0e9e405eb073b2ad8ad7
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2024-01-18 09:54:16 +0100

    correction orthographe globale

--- portfolio-product-owner/src/components/work/Work.jsx
+++ portfolio-product-owner/src/components/work/Work.jsx
@@ -6,7 +6,10 @@ const Work = () => {
     return (
         <section className="work section" id="portfolio">
             <h2 className="section__title">Portfolio</h2>
-            <span className="section__subtitle"> Mes derniers projets </span>
+            <span className="section__subtitle"> 
+                Mes derniers projets dans le cadre de ma formation <br/>
+                de Product manager chez OpenClassrooms 
+            </span>
             <Works/>
         </section>
     )
